b92372e74df0badf6f66e9e3feb0861e3d3e8a17
Catalin-Apetroaei <apetroaei.catalin@yahoo.com>
2023-06-05 15:07:49 +0300

    commit-tema

--- Tema1/script.js
+++ Tema1/script.js
@@ -2,26 +2,28 @@
 
 // Tema 1: JS Array
 
-const initialValues = [
-  1,
-  2,
-  "salut",
-  "Catalin",
-  [3, 4, 5],
-  true,
-  { nume: "Apetroaei", age: 39 },
-];
-const otherValues = [
-  10,
-  11,
-  12,
-  "Ingrid",
-  "Simona",
-  "Maria",
-  [13, 14],
-  false,
-  { locatie: "Iasi", buletin: true },
-];
+const initialValues = [1, 2, "salut", "Catalin", [3, 4, 5], true];
+const otherValues = [10, 11, 12, "Ingrid", "Simona", "Maria", [13, 14], false];
+// 2)
 initialValues.push(otherValues);
+// 3 , 4)
+initialValues[0] = initialValues[0] + 2;
+initialValues[1] = initialValues[1] + 2;
+initialValues[4][0] = initialValues[4][0] + 2;
+initialValues[4][1] = initialValues[4][1] + 2;
+initialValues[4][2] = initialValues[4][2] + 2;
+initialValues[6][0] = initialValues[6][0] + 2;
+initialValues[6][1] = initialValues[6][1] + 2;
+initialValues[6][2] = initialValues[6][2] + 2;
+initialValues[6][6][0] = initialValues[6][6][0] + 2;
+initialValues[6][6][1] = initialValues[6][6][1] + 2;
+// 5)
+initialValues[2] = initialValues[2] + "happy codding";
+initialValues[3] = initialValues[3] + "happy codding";
+initialValues[6][3] = initialValues[6][3] + "happy codding";
+initialValues[6][4] = initialValues[6][4] + "happy codding";
+initialValues[6][5] = initialValues[6][5] + "happy codding";
+// 6)
+initialValues[5] = !initialValues[5];
+initialValues[6][7] = !initialValues[6][7];
 console.log(initialValues);
-console.log("continuam");
