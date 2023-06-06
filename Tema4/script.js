79e7a06a9e175ef302928b7d9e278c868d259916
Catalin-Apetroaei <apetroaei.catalin@yahoo.com>
2023-06-06 18:00:12 +0300

    commit2

--- Tema4/script.js
+++ Tema4/script.js
@@ -6,22 +6,45 @@ Creati o functie care sa calculeze venitul total anual in lei al unei persoane p
 */
 
 const firstSentence = `Ion castiga 5000 lei/luna din salariu, 10000 lei/an bonus si 1500 lei/luna din freelancing.`;
-const secondSentence = `Andrei are un salariu de 1000 euro/luna, un bonus de 9000 de lei/an si mai castiga suplimentar aproximativ 500 usd/luna din freelancing.`;
+const secondSentence = `Andrei are un salariu de 1000 euro/luna, un bonus de 9000 lei/an si mai castiga suplimentar aproximativ 500 usd/luna din freelancing.`;
 
-const venitAnual = function (sentence) {
-  const venit = sentence.split(" ");
-  let total = 0;
-  for (let valoare of venit) {
-    const num = function (val) {
-      return /[0-9]/.test(val);
-    };
-    // console.log(num(valoare));
-    if (num(valoare)) {
-      //   console.log(valoare);
-      total = total + Number(valoare);
+// const venitAnual = function (sentence) {
+//   const venit = sentence.split(" ");
+//   let total = 0;
+//   for (let valoare of venit) {
+//     const num = function (val) {
+//       return /[0-9]/.test(val);
+//     };
+//     // console.log(num(valoare));
+//     if (num(valoare)) {
+//       //   console.log(valoare);
+//       total = total + Number(valoare);
+//     }
+//   }
+//   console.log(total);
+//   return `Venitul total anual al lui ${venit[0]} este de ${total} lei`;
+// };
+
+// console.log(venitAnual(firstSentence));
+// console.log(venitAnual(secondSentence));
+const venit = function (sentence) {
+  const arr = sentence.split(" ");
+  let venitAnual = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "lei/luna") {
+      venitAnual += Number(arr[i - 1]) * 12;
+    }
+    if (arr[i] === "lei/an") {
+      venitAnual += Number(arr[i - 1]);
+    }
+    if (arr[i] === "euro/luna") {
+      venitAnual += Number(arr[i - 1]) * 4.96;
+    }
+    if (arr[i] === "usd/luna") {
+      venitAnual += Number(arr[i - 1]) * 4.64;
     }
   }
-  console.log(total);
-  return `Venitul total anual al lui ${venit[0]} este de ${total} lei`;
+  console.log(venitAnual);
 };
-console.log(venitAnual(firstSentence));
+venit(firstSentence);
+venit(secondSentence);
