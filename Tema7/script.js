48fcb04846e0f617cd30d8a5df6f9e1a7e8e2c0b
Catalin-Apetroaei <apetroaei.catalin@yahoo.com>
2023-06-20 17:52:43 +0300

    Am terminat tema7

--- Tema7/script.js
+++ Tema7/script.js
@@ -5,22 +5,23 @@ const items = document.getElementById("addItem");
 const addItem = document.getElementById("add");
 
 const newItems = function () {
-  const li = document.createElement("li");
-  const button = document.createElement("button");
-  const input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
-  button.setAttribute("id", "erase");
-  let message = items.value.split("");
-  if (message.length > 2) {
-    ul.appendChild(li);
-    li.textContent = message.join("");
-    li.appendChild(button);
-    li.appendChild(input);
-  } else alert("Textul introdus trebuie sa aiba cel putin 3 caractere!");
-  items.value = "";
-  const eraseButton = document.getElementById("erase");
-  const eraseItem = eraseButton.addEventListener("click", function (e) {
-    console.log(e);
-  });
+  if (listLi.length < 10) {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    const input = document.createElement("input");
+    input.setAttribute("type", "checkbox");
+    button.textContent = "X";
+    let message = items.value.split("");
+    if (message.length > 2) {
+      ul.appendChild(li);
+      li.textContent = message.join("");
+      li.appendChild(button);
+      li.appendChild(input);
+    } else alert("Textul introdus trebuie sa aiba cel putin 3 caractere!");
+    items.value = "";
+    const eraseItem = button.addEventListener("click", function () {
+      li.remove();
+    });
+  } else alert("Ati atins numarul maxim de repere introduse!");
 };
 const addItems = addItem.addEventListener("click", newItems);
